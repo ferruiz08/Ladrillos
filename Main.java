c3c05202f9a3a19fa56b94191828d4f63846d88a
Fer Ruiz <54475748+ferruiz08@users.noreply.github.com>
2019-09-15 19:12:36 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package ladrillos;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException  {
+		// TODO Auto-generated method stub
+		
+		Pared pared = new Pared("C:\\Users\\Fer\\eclipse-workspace\\Ladrillos\\pared.in");
+		pared.imprimir();
+		
+	}
+
+}
